6560c8ad1e8e933250d52f84b3caea16d3f763b2
Kenneth Griggs <kenrgriggs@gmail.com>
2021-04-05 21:55:19 -0400

    I'm going to bed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 import './App.css';
 
+interface FormData {
+  email: String
+  password: string
+  source: SourceEnum
+  remember: boolean
+  
+}
+
 function App() {
+
+  const [ name, setName ] 
+
   return (
     <div>
       Hey
